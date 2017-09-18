ee82e18dca54c67d2ff8962f31c7d90c8f7b12fa
T <thomassocska@yahoo.com>
2017-09-18 19:36:34 +0200

    Add analysis script

--- analysis_script.R
+++ analysis_script.R
@@ -1,3 +1,5 @@
+options(encoding = "UTF-8")
+
 library(MASS)
 library(tidyverse)
 library(magrittr)
@@ -19,6 +21,7 @@ senti <- bind_rows(senti_pos, senti_neg)
 
 films <- c("FD_0CU", "FD_5CU", "FD_10CU")
 film_titles <- c("0 close-ups", "5 close-ups", "10 close-ups")
+cu_levels <- c("No close-ups", "Contains close-ups")
 
 # Transform data
 seen <- 
@@ -27,7 +30,8 @@ seen <-
         filter(stimulus %in% films) %>% 
         drop_na() %>% 
         select(-seen) %>% 
-        mutate(stimulus = stimulus %>% factor(., levels = films, labels = film_titles))
+        mutate(Close_up = if_else(stimulus == films[1], cu_levels[1], cu_levels[2]) %>% factor(levels = cu_levels),
+               stimulus = stimulus %>% factor(., levels = films, labels = film_titles))
 
 df <- 
         tom_df %>% 
@@ -35,22 +39,25 @@ df <-
         
 # Showing densities
 df %>% 
-        select(stimulus, Affective_female:Intention_male) %>% 
-        gather(tom_type, value, -stimulus) %>% 
+        select(Close_up, Affective_female:Intention_male) %>% 
+        gather(tom_type, value, -Close_up) %>% 
         ggplot() +
         aes(x = value, y = tom_type) +
         geom_joy() +
-        facet_wrap(~stimulus) +
+        facet_wrap(~Close_up) +
         ggtitle("Density of tom categories by movie version")
 
 glm_aff_fem <- glm(Affective_female ~ stimulus + offset(log(Length)), family = "poisson",data = df)
+summary(glm_aff_fem)
+glm_aff_fem <- glm(Affective_female ~ Close_up + offset(log(Length)), family = "poisson",data = df)
 summary(glm_aff_fem)
 AER::dispersiontest(glm_aff_fem, alternative = "greater")
 
 # We have to run a negative binomial regression, since dispersion is 1.9 (variance is almost 2x the mean). This parameter was calculated using quasipoisson family.
 glm_nb_aff_fem <- glm.nb(Affective_female ~ stimulus + offset(log(Length)), data = df)
+glm_nb_aff_fem <- glm.nb(Affective_female ~ Close_up + offset(log(Length)), data = df)
 summary(glm_nb_aff_fem)
-
+anova(glm_nb_aff_fem)
 
 df %>% 
         group_by(stimulus) %>% 
@@ -59,9 +66,11 @@ df %>%
         ggplot(data = .) +
                 aes(x = stimulus, y = Mean, ymin = Mean - Se, ymax = Mean + Se) +
                 geom_errorbar(width = .5) +
-                geom_col()
-
-
+                geom_col() +
+                theme_minimal() +
+                labs(x = NULL, y = "Affective mentalizaton to the female character") +
+                ggtitle("Differences in the mean affective mentalization response to the female character (SE)")
+        
 
 ## Preparing the text for sentiment analysis
 # The text is already annotated by magyarlanc API, so we just load the files
@@ -81,11 +90,14 @@ words <-
                                stem
                                ),
                 corrected = !spelling
-        ) %>% 
+        ) %>%
         left_join(senti, by = "stem") %>% # Add sentiment lexicon
-        select(-file, -full_annot, -spelling) %>% 
+        select(-file, -full_annot, -spelling) %>%
         group_by(TestId)
+# TODO: Correct the words BEFORE taking apart with magyarlanc
 
+
+# Average setiment by stimuli
 words %>%
         summarise(sentiment = sum(sentiment, na.rm = T)) %>% # Add up sentiments into one num/tester
         left_join(df, by = "TestId") %>% 
@@ -93,24 +105,41 @@ words %>%
         summary()
 
 words %>%
+        select(TestId, word, sentiment) %>% 
+        drop_na(sentiment) %>% 
+        mutate(sentiment = recode(sentiment, `1` = "positive", `-1` = "negative")) %>% 
+        left_join(seen) %>% 
+        group_by(TestId, stimulus) %>% 
+        count(sentiment) %>% 
+        spread(sentiment, n) %>% 
+        mutate_at(vars(negative, positive), funs(if_else(is.na(.), 0L, .))) %>% 
+        left_join(tom_df %>% select(TestId, Length)) %>% 
+        glm.nb(positive ~ stimulus + offset(log(Length)), data = .) %>%
+        summary()
+        
+        
+words %>%
         summarise(sentiment = sum(sentiment, na.rm = T)) %>% # Add up sentiments into one num/tester
         left_join(df, by = "TestId") %>% 
         ggplot() +
                 aes(y = stimulus, x = sentiment) +
                 geom_joy() +
-                ggtitle("Density of sentiment scores by film version")
+                ggtitle("Density of sentiment scores by film version") +
+                theme_minimal()
 
 words %>%
         summarise(sentiment = sum(sentiment, na.rm = T)) %>% # Add up sentiments into one num/tester
         left_join(df, by = "TestId") %>% 
-        group_by(stimulus) %>% 
+        group_by(Close_up, stimulus) %>% 
         summarise(Mean = mean(sentiment, na.rm = T),
                   Se = sd(sentiment, na.rm = T)/sqrt(n())) %>% 
         ggplot() +
                 aes(x = stimulus, y = Mean, ymax = Mean + Se, ymin = Mean - Se) +
                 geom_errorbar(width = .5) +
                 geom_col() +
-                ggtitle("Average sentiment scores by film version")
+                ggtitle("Average sentiment scores by film version") +
+                theme_minimal() +
+                labs(x = NULL, y = "Mean sentiment score")
 
 words %>% 
         count(sentiment) %>% # Count the sentiment data (separately for both sentiments)
@@ -133,9 +162,12 @@ words %>%
         drop_na(sentiment) %>% 
         ggplot() +
                 aes(x = stimulus, y = Mean, ymax = Mean + Se, ymin = Mean - Se, group = sentiment, fill = sentiment) +
-                geom_errorbar(position = "dodge") +
-                geom_col(position = "dodge") +
-                ggtitle("Average sentiment scores by film version, separately for sentiments")
+                geom_errorbar(position = "dodge", width = .5) +
+                geom_col(position = "dodge", width = .5) +
+                ggtitle("Average sentiment scores by film version, separately for sentiments") +
+                labs(x = NULL, y = "Mean aggregated sentiment") +
+                theme_minimal()
+
 
 
 
